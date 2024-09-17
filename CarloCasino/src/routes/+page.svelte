7849ac42f7a177caaf89a9445b2d9d5f560463ca
Carlo <davidcarlanton06@gmail.com>
2024-09-17 11:13:55 +0200

    home page layout

---
+++ CarloCasino/src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+    import "../app.css";
+</script>
+
+<div id="main" class="flex flex-col items-center bg-black w-screen h-screen">
+    <div id="navbar" class="flex justify-evenly items-center w-full h-1/6 bg-navy">
+        <a href="/scoreboard" class="flex justify-center btn items-center w-1/5 md:w-1/6 h-1/4 rounded-lg font-mono text-xs md:text-xl bg-blue">
+            Scoreboard
+        </a>
+        <div class="flex justify-center items-center w-2/6 h-1/4 md:w-1/6 md:h-2/5 rounded-lg bg-blue font-mono text-base md:text-3xl">
+            Penger 0
+        </div>
+          <a href="/login" class="flex justify-center btn items-center w-1/6 md:w-1/12 h-1/4 rounded-lg bg-blue font-mono text-xs md:text-xl">
+              Logg inn 
+          </a>
+          <div class="flex justify-center items-center w-1/6 md:w-1/12 h-1/4 rounded-lg bg-blue font-mono text-xs md:text-xl">
+              User
+          </div>
+    </div>
+    <div id="resten" class="flex flex-col  justify-evenly items-center w-full h-5/6">
+        <div id="rain" class="flex justify-between items-center pl-4 pr-4 w-5/6 h-1/6 rounded-lg bg-navy">
+            <span class="flex items-center justify-center w- bg-blue border-2 rounded-lg font-mono border-black">Du kan få GRATIS PENGER hvert 30. Minutt</span>
+            <div class="flex items-center justify-center w-1/6 h-4/6 bg-blue border-2 rounded-lg font-mono text-5xl border-black">30:00</div>
+            <div class="flex items-center justify-between p-4 bg-blue h-5/6 w-1/6 border-2 rounded-lg">
+                <img class="w-3/12" src="/Gold_Coins.png" alt="coin">
+                <span class=" font-mono text-6xl">50</span>
+            </div>
+            <button class="flex justify-center btn items-center w-1/6 h-3/4 rounded-lg bg-blue border-2 border-black text-5xl font-mono">Claim</button>
+        </div>
+        <div id="spill" class="flex justify-evenly w-full h-4/6">
+            <div id="button" class="flex items-center justify-center rounded-lg w-3/12 h-full">
+                <img class="rounded-lg h-11/12 w-11/12" src="/blackjack.png" alt="blue button">
+            </div>
+            <div id="button" class="flex items-center justify-center rounded-lg w-3/12 h-full">
+                <img class="rounded-lg h-11/12 w-11/12" src="/blue_button.png" alt="blue button">
+            </div>
+        </div>
+    </div>
+</div>
+<style>
+    .btn {
+      transition: transform 0.05s ease;
+    }
+    .btn:active {
+      transform: scale(0.95);
+    }
+  </style>
---
+++ CarloCasino/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    colors: {
+      'black': '#1B262C',
+      'navy': '#0F4C75',
+      'blue': '#3282B8',
+      'sky': '#BBE1FA',
+    },
+  },
+  plugins: []
+};
